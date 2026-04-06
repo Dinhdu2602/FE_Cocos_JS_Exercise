99d4913a7bfbd7b68f3eb319432aeb0fd4584a2f
Dinh Du <20520159@gm.uit.edu.vn>
2026-04-06 20:32:38 +0700

    Switch folder Exercise: 02_04_2026 to branch 02_04_2026

--- 03_04_2026/Library_Management/models/book.js
+++
@@ -1,7 +0,0 @@
-function Book(id, title, author) {
-    this.id = id;
-    this.title = title;
-    this.author = author;
-    this.isBorrowed = false;
-}
-module.exports= Book;
--- 03_04_2026/Library_Management/utils/idGenerator.js
+++
@@ -1,9 +0,0 @@
-function createIdGenerator() {
-    var currentId = 0;
-
-    return function() {
-        currentId = currentId + 1;
-        return currentId;
-    };
-}
-module.exports = createIdGenerator;
